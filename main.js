41036d247514fe926a967ca484f41a92659b941d
Dmytro Frolov <dmytro.frolov@sigma.software>
2021-07-25 20:19:33 +0300

    implement one by one sent and status filter

--- main.js
+++ main.js
@@ -1,31 +1,71 @@
 const dom = {
-    button: document.getElementById('getUsers'),
+    buttonParallel: document.getElementById('getUsersParallel'),
+    buttonOneByOne: document.getElementById('getUsersOneByOne'),
+    buttonOneByOneSuccess: document.getElementById('getUsersOneByOneSuccess'),
     userList: document.getElementById('userList'),
 }
 
 const generateUsersRequests = () => [...Array(10)].map(() => {
-    const randomUserId = Math.floor(Math.random() * 15) + 1
+    const randomUserId = Math.floor(Math.random() * 13) + 1
     return `https://jsonplaceholder.typicode.com/users/${randomUserId}`
 })
 
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+const getRequests = ({ requests = [], isParallelDispatch = true }) => {
+    if (isParallelDispatch) {
+        return Promise
+            .all(requests.map(request => fetch(request)))
+            .then(responses => {
+                const hasErrors = !!responses.find(request => request.status === 404)
+                return responses
+            })
+    } else {
+        const result = []
 
-const getRequests = ({ requests = [], isParallelDispatch = true, shouldShowPartResult = true }) => Promise
-    .all(requests.map(request => fetch(request)))
-    .then(responses => {
-        const hasErrors = !!responses.find(request => request.status === 404)
-        if (!shouldShowPartResult && hasErrors) return null
-        return responses
-    })
-
-
-async function renderRequests() {
-    const responses = await getRequests({ requests: generateUsersRequests() })
-    const responsesListElement = responses.map(response => `<li>${response.url} - ${response.ok ? '✅' : '❌'}</li>`)
-
-    dom.userList.innerHTML = `<ul>${responsesListElement}</ul>`
-    console.log(responses)
+        const getRequest = async (requests) => {
+            console.log()
+            const a = await fetch(requests[0]).then(async (res) => {
+                if (requests.length > 1) {
+                    requests.shift()
+                    if (res.ok) {
+                        await getRequest(requests)
+                    } else {
+                        result.push(...requests.map(request => ({
+                            url: request,
+                            ok: false,
+                            notSent: true
+                        })))
+                    }
+                }
+                return res
+            })
+            result.push(a)
+            return result
+        }
+        return getRequest(requests)
+    }
 
 }
 
-dom.button.addEventListener('click', renderRequests)
+
+async function renderRequests({ isParallelDispatch, shouldShowWithErrors }) {
+    const responses = await getRequests({ requests: generateUsersRequests(), isParallelDispatch })
+    console.log(responses)
+    const responsesListElement = responses
+        .filter(response => shouldShowWithErrors ? response : response.ok)
+        .reverse()
+        .map(response => `<li>
+            ${response.url} - 
+            ${response.ok ? '✅' : response.notSent ? '⛔' : '❌'} - 
+            status: ${response.ok ? 'success' : 'failed'}
+        </li>`)
+
+    dom.userList.innerHTML = `<ul>${responsesListElement}</ul>`
+}
+
+dom.buttonParallel.addEventListener('click', () => renderRequests({ isParallelDispatch: true, shouldShowWithErrors: true }))
+dom.buttonOneByOne.addEventListener('click', () => renderRequests({ isParallelDispatch: false, shouldShowWithErrors: true }))
+dom.buttonOneByOneSuccess.addEventListener('click', () => renderRequests({ isParallelDispatch: false, shouldShowWithErrors: false }))
